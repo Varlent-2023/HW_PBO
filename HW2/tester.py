cf6db3d3fd670403aaf1539846fd6f0c55d1cdf9
Varlent-2023 <151530434+Varlent-2023@users.noreply.github.com>
2024-04-23 23:20:23 +0700

    Update

--- HW2/tester.py
+++ HW2/tester.py
@@ -10,10 +10,7 @@ class User:
         self._user_type = user_type
 
     def login(self, user_id, password):
-        # if user_id == self._user_id and password == self._password:
         return True
-        # else:
-        #     return False
 
     def get_user_id(self):
         return self._user_id
